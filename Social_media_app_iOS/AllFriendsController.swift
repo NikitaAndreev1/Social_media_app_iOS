ef41c9adb4e1a82711c39b964220d2e6a48f623d
Nikita Andreev <nikita.andreev.work@gmail.com>
2020-08-07 09:57:51 +0300

    Changed row height

--- Social_media_app_iOS/AllFriendsController.swift
+++ Social_media_app_iOS/AllFriendsController.swift
@@ -20,24 +20,30 @@ class AllFriendsController: UITableViewController {
 
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-      
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//      
+//    }
+//
+//    // MARK: - Table view data source
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//           let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsCell", for: indexPath) as! FriendsCell
+//           cell.cityLabel.text = cities[indexPath.row]
+//           return cell
+//       }
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
