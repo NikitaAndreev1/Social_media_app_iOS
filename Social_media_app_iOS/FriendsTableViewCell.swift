60d8ad00889e2f7f68378fb9f5e8401195c9fc35
Nikita Andreev <nikita.andreev.work@gmail.com>
2020-08-10 09:58:24 +0300

    added an image next to label

---
+++ Social_media_app_iOS/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  Lesson_1
+//
+//  Created by Nicks on 10.08.2020.
+//  Copyright © 2020 Nicks. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
